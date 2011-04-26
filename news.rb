c1d2ba030ce54d3364480c184fe04821ddde707e
Thomas Plotkowiak <plotti@gmx.net>
2011-04-27 00:09:18 +0200

    getting the tweets finally

--- news.rb
+++ news.rb
@@ -1,12 +1,10 @@
-require 'rubygems'
 require 'init'
 require 'open-uri'
 require 'readability'
-require 'sinatra'
 require 'digest'
 require 'pismo'
 
-# Returns strong ties [[id, strengthth],...] for a given user using the infochimp API
+# Returns strong ties [[id, strength],...] for a given user using the infochimp API
 def get_strong_ties_for(username)
 	begin 
 		result = YAML.load_file("/data/" + username + "_strongties")
@@ -26,19 +24,31 @@ def get_strong_ties_for(username)
 	end	        
 end
 
+# Returns all friends for a twitter user (friends == people a user follows)
 def get_friends_for(username)
-	friends = []
-	cursor = -1 
-	counter = 0
-	max = MAX_FRIENDS/100
-	while cursor != 0 && counter < max
-		result = Twitter.friends(username, {:cursor => cursor})
-		friends += result[:users]
-		cursor = result[:next_cursor]
-		puts "Next Cursor for #{username} is #{cursor}"
-		counter += 1
+	begin 
+		friends = YAML.load_file("/data/" + username + "_friends")
+	rescue
+		puts "Have to get all the friends for the user #{username} first"
 	end
-	return friends
+	if friends == nil
+		friends = []
+		cursor = -1 
+		counter = 0
+		#Dont get more friends than we think is necessary, default 1000
+		max = MAX_FRIENDS/100
+		while cursor != 0 && counter < max
+			result = Twitter.friends(username, {:cursor => cursor})
+			friends += result[:users]
+			cursor = result[:next_cursor]
+			puts "Next Cursor for #{username} is #{cursor}"
+			counter += 1
+		end
+		File.open("/data/" + username + "_friends","w") {|file| file.puts(friends.to_yaml)}
+		return friends
+	else
+		return friends
+	end
 end
 
 # Computes Indegree Measures for members of the egonetwork of a given user
@@ -76,6 +86,7 @@ def get_centralities_for(username)
 	end
 end
 
+#Gets the Content for a given url using the readability project
 def get_content_for(tweets)
 	tweets.each do |tweet|
 		begin
@@ -87,14 +98,15 @@ def get_content_for(tweets)
 			puts "Have to obtain content for this url first"
 		end
 		if file == nil
+		    # We are using Pismo Gem instead now
 			#text = open(tweet[:uri]).read rescue ""
 			#tweet[:content] = Readability::Document.new(text).content(remove_unlikely_candidates=true) rescue ""
 	    	#tweet[:title] = Nokogiri::HTML(text).at_css("title").text  rescue ""
 		    #tweet[:image] = ""
 		    begin 
 		    	doc = Pismo::Document.new(tweet[:uri])
-		    	tweet[:content] = doc.body
-		    	tweet[:title] = doc.title
+		    	tweet[:content] = doc.html_body
+		    	tweet[:title] = doc.html_title
 		    	tweet[:image] = doc.images.first rescue ""
 		    	tweet[:icon] = doc.favicon rescue ""
 		    rescue
@@ -109,6 +121,9 @@ def get_content_for(tweets)
 	return tweets
 end
 
+# Returns all retweets for a given tweet
+# This operation takes quite long
+#TODO The cached version of retweets is not up to date there might have been more changes but we dont know about them untill we recompute
 def get_retweets_for(tweets)
 	i = 0
 	tweets.each do |tweet|
@@ -117,8 +132,8 @@ def get_retweets_for(tweets)
 		rescue
 		end
 		if tweet[:retweet_ids] == nil
+			puts "Have to calculate Retweets for #{i} of #{tweets.count}"		
 			i += 1
-			puts "Calculating #{i} of #{tweets.count}"
 			tweet[:retweet_ids] = []
 			Twitter.retweeters_of(tweet.id, {:count => 100}).each do |retweet|
 				tweet[:retweet_ids] << { :id => retweet.id, :person => retweet.screen_name, :published_at => retweet.created_at}
@@ -129,6 +144,7 @@ def get_retweets_for(tweets)
 	return tweets
 end
 
+# Gets the link of a tweet
 def get_links_for(tweets)
 	url_regexp = /http:\/\/\w/
 	result = []
@@ -142,10 +158,29 @@ def get_links_for(tweets)
 	return result
 end
 
+# Gets enough relevant tweets for a user
+# This operation takes quite long
+#TODO the cached version needs to be updated every couple of hours etc... maybe individually based on the rate of each tweeter
 def get_tweets_for(username)
-	tweets = Twitter.home_timeline(:count => 500)
+	friends = get_friends_for(username)
+	tweets = []
+	friends.each do |friend|
+	    # We will get 20 most recent tweets from each person we follow
+		begin 
+			result = YAML.load_file("/data/" + friend.screen_name + "_tweets")
+		rescue
+			puts "Collecting tweets of #{friend.screen_name}"
+		end
+		if result == nil
+			result = Twitter.user_timeline(friend.screen_name)
+			File.open("/data/" + friend.screen_name + "_tweets","w") {|file| file.puts(result.to_yaml)}
+		end
+		tweets += result 
+	end
+	return tweets
 end
 
+# Calculates a recency value for a set of tweets. Basically a value between 0 and 1 depending how old the tweet is
 def get_news_value_for(tweets)
 	#News value is only based on recency so far
 	tweets.each do |tweet|
@@ -153,10 +188,12 @@ def get_news_value_for(tweets)
 	end
 end
 
+# Calculates all the Values for the newspaper
 def calculate_newspaper(tweets,strong_ties,centralities)
 	 max_retweets = 0
 	 max_centrality = 0
 	 max_strong = 0
+	 max_news = 0
 	 #Normalization of Values to 1
      centralities.each do |centrality|
      	if centrality[1] > max_centrality
@@ -167,6 +204,9 @@ def calculate_newspaper(tweets,strong_ties,centralities)
      	if tweet[:retweet_ids].count > max_retweets
      		max_retweets = tweet[:retweet_ids].count
      	end
+     	if tweet[:news_value] > max_news
+     		max_news = tweet[:news_value]
+     	end
      end
      strong_ties.each do |tie|
      	if tie[1] > max_strong
@@ -174,69 +214,17 @@ def calculate_newspaper(tweets,strong_ties,centralities)
      	end
      end
      tweets.each do |tweet|
-        rt_score = (tweet[:retweet_ids].count / max_retweets).to_f
+        rt_score = (tweet[:retweet_ids].count.to_f / max_retweets)
         st_score = (strong_ties[tweet.user.id].to_f/ max_strong)
         ct_score = (centralities[tweet.user.screen_name].to_f / max_centrality)
-        nw_score = tweet[:news_value]
+        nw_score = (tweet[:news_value].to_f / max_news)
+        tweet[:rt_score] = rt_score
+        tweet[:st_score] = st_score
+        tweet[:ct_score] = ct_score
+        tweet[:nw_score] = nw_score
 		score = rt_score + st_score + ct_score + nw_score
 		tweet[:score_string] = "Total Score: #{score} RT: #{rt_score} ST: #{st_score} CT: #{ct_score} NW: #{nw_score}"
 		tweet[:score] = score
      end
      return tweets
 end
-
-get '/show/:username' do |username|
-	@username = username
-	puts "Calculating strongies"
-	strong_ties = get_strong_ties_for(username)
-	puts "Calculating Centralities"
-	centralities = get_centralities_for(username)
-	puts "Getting tweets"
-	result = get_tweets_for(username)
-	puts "Filtering only to tweets with links"
-	result = get_links_for(result)
-	puts "Getting retweets for tweets"
-	result = get_retweets_for(result)
-	puts "Getting news value for tweets"
-	result = get_news_value_for(result)
-	puts "Calculating newspaper"
-	result = calculate_newspaper(result,strong_ties,centralities)
-	puts "Getting content for best 20 items"
-	result = result.sort!{|a,b| a[:score] <=> b[:score]}.reverse[0..20]
-	@news = get_content_for(result)
-	haml :show
-end
-
-__END__
-@@layout
-%html
-  <?xml version="1.0" encoding="UTF-8"?>
-  %header
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-  %title="The #{@username} Times"
-  %body
-    %h1="The #{@username} Times"
-    %content=yield
-
-
-@@ show
-- @news.each do |item|
-  %p
-    %h1
-      -if item[:icon] != ""
-        %img{:src => item[:icon]}
-      %a{:href => item[:uri]}
-        =item[:title]
-    ="Tweet: #{item[:text]} (#{item[:created_at]}). "
-    %br
-    %b="Tweeted by: #{item[:user][:screen_name]}"
-    %br
-    %b="#{item[:score_string]}."
-    %br
-    %b="Retweeted by #{item[:retweet_ids].count}"    
-  %p
-    - if item[:image] != ""
-      %p
-        %img{:src => item[:image]}
-    =item[:content]
-  %hr
---
+++ web.rb
@@ -0,0 +1,68 @@
+require 'rubygems'
+require 'sinatra'
+require 'news'
+
+get '/show/:username' do |username|
+	@username = username
+	puts "Calculating strongies"
+	strong_ties = get_strong_ties_for(username)
+	puts "Calculating Centralities"
+	centralities = get_centralities_for(username)
+	puts "Getting tweets"
+	result = get_tweets_for(username)
+	puts "Filtering only to tweets with links"
+	result = get_links_for(result)
+	puts "Getting retweets for tweets"
+	result = get_retweets_for(result)
+	puts "Getting news value for tweets"
+	result = get_news_value_for(result)
+	puts "Calculating newspaper"
+	result = calculate_newspaper(result,strong_ties,centralities)
+	puts "Getting content for best 20 items"
+	result = result.sort!{|a,b| a[:score] <=> b[:score]}.reverse[0..20]
+	@news = get_content_for(result)
+	haml :show
+end
+
+__END__
+@@layout
+%html
+  <?xml version="1.0" encoding="UTF-8"?>
+  %header
+    %link{ :rel => "stylesheet", :type => "text/css", :href => "/reset.css"}
+    %link{ :rel => "stylesheet", :type => "text/css", :href => "/style.css"}
+    %link{ :rel => "stylesheet", :type => "text/css", :href => "/typography.css"}
+    %meta{ "http-equiv" => "Content-Type", :content => "text/html; charset=utf-8"}
+  %title="The #{@username} Times"
+  %body
+    .main
+      %h1="The #{@username} Times"
+      =yield
+
+
+@@ show
+- @news.each do |item|
+  #separator
+  %p
+    %h2
+      -if item[:icon].to_s.lstrip.rstrip != ""
+        %img{:src => item[:icon], :width => 50, :height => 50}
+      %a{:href => item[:uri]}
+        =item[:title]
+    %blockquote
+      ="Tweet: #{item[:text]} (#{item[:created_at]}). "
+    
+    %h3
+      Tweeted by:
+      %a{:href => "http://twitter.com/#{item[:user][:screen_name]}/status/#{item[:id_str]}"}
+        =item[:user][:screen_name]
+    %h4="Retweeted by #{item[:retweet_ids].count}"    
+    - total_score_string = "http://chart.apis.google.com/chart?chxr=0,0,4&chxt=y&chs=300x150&cht=gm&chd=t:#{item[:score]}&chds=0,4&chtt=Total+Value"
+    %img{ :src => total_score_string, :width => "300", :height => "150", :alt => "Tweet Value"}
+    - pre = "http://chart.apis.google.com/chart?chbh=a,6,20&chs=300x150&cht=bhg&chco=FF0000,00FF00,FF9900,0000FF&chds=0,1,0,1,0,1,0,1&chd="
+    - val = "t:#{item[:nw_score]}|#{item[:rt_score]}|#{item[:ct_score]}|#{item[:st_score]}"
+    - suff = "&chm=tRecency,000000,0,0,13|tRetweets,000000,1,1,13,-1|tAuthority,000000,2,0,13|tFriend+Value,000000,3,1,13"
+    %img{ :src => "#{pre+val+suff}", :width => "300", :height => "150", :alt => "" }
+  %p
+    =item[:content]
+  %hr
